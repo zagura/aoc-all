10a38bbe4a8fbca1478ec80a684c07e7cb7573e7
Michał Zagórski <zagura6@gmail.com>
2021-01-02 17:44:10 +0100

    Day 23 in C++. Slow version for part 2

--- 2020/23day/cpp/task1.cpp
+++ 2020/23day/cpp/task1.cpp
@@ -3,10 +3,10 @@
  *
  *       Filename:  task1.cpp
  *
- *    Description:  Advent of Code 2020 - Day 13
+ *    Description:  Advent of Code 2020 - Day 23
  *
  *        Version:  0.1.0
- *        Created:  13.12.2020
+ *        Created:  23.12.2020
  *
  *         Author:  Michał Zagórski (zagura), <zagura6@gmail.com>
  *
@@ -19,15 +19,63 @@
 #include <algorithm>
 #include <array>
 #include <map>
+#include <list>
 #include <vector>
 
+constexpr size_t kCupsSize = 9;
+constexpr int kRoundCount = 100;
 using std::string;
 using std::vector;
 using std::array;
 using std::map;
 using std::stringstream;
+using cups = std::list<int>;
 
+cups::iterator get_next(cups& l, cups::iterator& current) {
+    return (std::next(current)  == l.end() ? l.begin() : std::next(current));
+}
 
+void next_move(cups& round_cups, cups::iterator& current_iter) {
+    int current_cup = *current_iter;
+    printf("cups:");
+    for (auto cup: round_cups) {
+        if (*current_iter == cup) {
+            printf(" (%d)", cup);
+        } else {
+            printf(" %d", cup);
+        }
+    }
+    printf("\npickup: ");
+    cups::iterator next = current_iter;
+    next = get_next(round_cups, next);
+    vector<int> pick_up {};
+    for (int i = 0; i < 3; i++) {
+        // Wrap around
+        if (next == round_cups.end()) {
+            next = round_cups.begin();
+        }
+        pick_up.push_back(*next);
+        next = round_cups.erase(next);
+        printf("%d, ", pick_up.back());
+    }
+    int destination = current_cup - 1;
+    if (destination == 0) {
+        destination = kCupsSize;
+    }
+    while (std::find(pick_up.begin(), pick_up.end(), destination) != pick_up.end()) {
+        destination--;
+        if (destination < 1) {
+            destination = kCupsSize;
+        }
+    }
+    auto iter = std::find(round_cups.begin(), round_cups.end(), destination);
+    iter = get_next(round_cups, iter);
+    round_cups.insert(iter, pick_up.begin(), pick_up.end());
+    current_iter = std::find(round_cups.begin(), round_cups.end(), current_cup);
+    current_iter = get_next(round_cups, current_iter);
+    printf("\ndestination: %d\n\n", destination);
+
+}
 
 int main(int argc, char* argv[]) {
     std::ifstream input { "input.in" };
@@ -38,20 +86,26 @@ int main(int argc, char* argv[]) {
         ::fprintf(stderr, "Cannot open input file\n");
         return 2;
     }
-    int result = 0;
-/*
-    for (string line; getline(input, line); ) {
 
+    string line;
+    getline(input, line);
+    cups v {};
+    for (char c: line) {
+        v.push_back(c - '0');
+    }
+    for (auto s = v.size() + 1; s <= kCupsSize; ++s) {
+        v.push_back(s);
+    }
+    auto iter = v.begin();
+    for (size_t round = 0; round < kRoundCount; round++) {
+        printf("-- move %zu --\n" , round + 1);
+        next_move(v, iter);
     }
 
- */
-
-/*  while (input >> dat) {
-        arr.push_back(data);
+    for(auto& cup: v) {
+        printf(" %d", cup);
     }
-*/
-
-    ::printf("Task 1 result: %d\n", result);
+    printf("\n");
     return 0;
 }
 
--- 2020/23day/cpp/task2.cpp
+++ 2020/23day/cpp/task2.cpp
@@ -3,10 +3,10 @@
  *
  *       Filename:  task1.cpp
  *
- *    Description:  Advent of Code 2020 - Day 13
+ *    Description:  Advent of Code 2020 - Day 23
  *
  *        Version:  0.1.0
- *        Created:  13.12.2020
+ *        Created:  01.01.2021 
  *
  *         Author:  Michał Zagórski (zagura), <zagura6@gmail.com>
  *
@@ -19,15 +19,64 @@
 #include <algorithm>
 #include <array>
 #include <map>
+#include <list>
 #include <vector>
-
+#include <cinttypes>
+constexpr int million = 1000 * 1000;
+constexpr size_t kCupsSize = million;
+constexpr int kRoundCount = 10 * million;
 using std::string;
 using std::vector;
 using std::array;
 using std::map;
 using std::stringstream;
+using cups = std::list<int>;
 
+cups::iterator get_next(cups& l, cups::iterator& current) {
+    return (std::next(current)  == l.end() ? l.begin() : std::next(current));
+}
 
+void next_move(cups& round_cups, cups::iterator& current_iter) {
+    int current_cup = *current_iter;
+//    printf("cups:");
+//    for (auto cup: round_cups) {
+//        if (*current_iter == cup) {
+//            printf(" (%d)", cup);
+//        } else {
+//            printf(" %d", cup);
+//        }
+//    }
+//    printf("\npickup: ");
+    cups::iterator next = current_iter;
+    next = get_next(round_cups, next);
+    vector<int> pick_up {};
+    for (int i = 0; i < 3; i++) {
+        // Wrap around
+        if (next == round_cups.end()) {
+            next = round_cups.begin();
+        }
+        pick_up.push_back(*next);
+        next = round_cups.erase(next);
+//        printf("%d, ", pick_up.back());
+    }
+    int destination = current_cup - 1;
+    if (destination == 0) {
+        destination = kCupsSize;
+    }
+    while (std::find(pick_up.begin(), pick_up.end(), destination) != pick_up.end()) {
+        destination--;
+        if (destination < 1) {
+            destination = kCupsSize;
+        }
+    }
+    auto iter = std::find(round_cups.begin(), round_cups.end(), destination);
+    iter = get_next(round_cups, iter);
+    round_cups.insert(iter, pick_up.begin(), pick_up.end());
+//    current_iter = std::find(round_cups.begin(), round_cups.end(), current_cup);
+    current_iter = get_next(round_cups, current_iter);
+//    printf("\ndestination: %d\n\n", destination);
+
+}
 
 int main(int argc, char* argv[]) {
     std::ifstream input { "input.in" };
@@ -38,20 +87,34 @@ int main(int argc, char* argv[]) {
         ::fprintf(stderr, "Cannot open input file\n");
         return 2;
     }
-    int result = 0;
-/*
-    for (string line; getline(input, line); ) {
 
+    string line;
+    getline(input, line);
+    cups v {};
+    for (char c: line) {
+        v.push_back(c - '0');
+    }
+    for (auto s = v.size() + 1; s <= kCupsSize; ++s) {
+        v.push_back(s);
+    }
+    auto iter = v.begin();
+    for (size_t round = 0; round < kRoundCount; round++) {
+        if (round % 10000 == 0) {
+            printf("Move: %zu", round);
+        }
+//        printf("-- move %zu --\n" , round + 1);
+        next_move(v, iter);
     }
 
- */
-
-/*  while (input >> dat) {
-        arr.push_back(data);
+    auto one = std::find(v.begin(), v.end(), 1);
+    uint64_t a = *get_next(v, one);
+    one = get_next(v, one);
+    uint64_t b = *get_next(v, one);
+    for(auto& cup: v) {
+        printf(" %d", cup);
     }
-*/
-
-    ::printf("Task 1 result: %d\n", result);
+    printf("\n");
+    printf("Part 2 result: %" PRIu64 "\n", a * b);
     return 0;
 }
 
