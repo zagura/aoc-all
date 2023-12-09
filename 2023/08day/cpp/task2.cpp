b09c9b7784a69cbba133889c60c3c257f98ebcd1
Michał Zagórski <zagura6@gmail.com>
2023-12-09 19:01:05 +0100

    AoC 2023: Day 8 in C++. Fix part 2 to use gcd and lcm to calculate result.

--- 2023/08day/cpp/task2.cpp
+++ 2023/08day/cpp/task2.cpp
@@ -7,12 +7,13 @@
  *                  --- Day 8: Haunted Wasteland ---
  *
  *        Version:  0.1.0
- *        Created:  08.12.2023
+ *        Created:  09.12.2023
  *
  *         Author:  Michał Zagórski (zagura), <zagura6@gmail.com>
  *
  * =====================================================================================
  */
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -30,45 +31,86 @@ struct Node {
     explicit Node(const std::string& n) : id(0), name(n), left(nullptr), right(nullptr) {}
 };
 
-bool verify(const std::vector<std::shared_ptr<Node>>& pos) {
-    for (const auto& p: pos) {
-        if (p) {
-            if (p->name.back() != 'Z') {
-                return false;
+std::map<std::string, std::shared_ptr<Node>>
+get_jumps(std::map<std::string, std::shared_ptr<Node>>& nodes, const std::string& pattern) {
+    std::map<std::string, std::shared_ptr<Node>> jumps {};
+    for (auto& [name, node]: nodes) {
+        jumps[name] = node;
+    }
+
+    for (const auto& c: pattern) {
+        for (auto& [name, node]: jumps) {
+            switch (c) {
+            case 'L':
+                node = node->left;
+                break;
+            case 'R':
+                node = node->right;
+                break;
             }
+        }
+    }
+
+    return jumps;
+}
+
+bool is_prime(size_t num) {
+    for (size_t i = 2; i * i <= num; i++) {
+        if (i != num && num % i == 0) {
+            return false;
         }
     }
     return true;
 }
 
-size_t traverse(std::map<std::string, std::shared_ptr<Node>>& nodes, const std::string& pattern) {
-    size_t step_count = 0;
-    std::vector<std::shared_ptr<Node>> curr {};
-    for (auto& [name, node]: nodes) {
+const std::string& next_step(const std::pair<std::string, std::shared_ptr<Node>>& el) {
+    return el.second->name;
+}
+
+// std::vector<int> get_single_paths()
+size_t get_path_len(const std::string& name, const std::map<std::string, std::shared_ptr<Node>>& jumps) {
+    size_t len = 1;
+    std::string n = name;
+    while (jumps.at(n)->name.back() != 'Z') {
+        len++;
+        n = jumps.at(n)->name;
+        std::cout << std::format("{}: {} -> {}\n", len, name, n);
+    }
+    return len;
+}
+
+size_t gcd(size_t left, size_t right) {
+    size_t greater = left;
+    size_t lower = right;
+    if (lower >= greater) {
+        std::swap(greater, lower);
+    }
+    while (greater != lower && greater % lower != 0) {
+        size_t tmp = greater % lower;
+        greater = lower;
+        lower = tmp;
+    }
+    return lower;
+
+}
+
+size_t lcm(size_t a, size_t b) {
+    return (a / gcd(a, b)) * b;
+}
+
+size_t count_paths(const std::map<std::string, std::shared_ptr<Node>>& jumps) {
+    std::vector<size_t> paths {};
+    for (const auto& [name, node]: jumps) {
         if (name.back() == 'A') {
-            curr.emplace_back(node);
+            paths.push_back(get_path_len(name, jumps));
+            std::cout << std::format("Start {} - len: {}\n", name, paths.back());
         }
     }
-    while (!verify(curr)) {
-        for (const auto& c: pattern) {
-            if (verify(curr)) break;
-            if (step_count % 10000) {
-                printf("Steps %zu\n", step_count);
-            }
-            step_count++;
-            for (auto& n: curr) {
-                switch (c) {
-                case 'L':
-                    n = n->left;
-                    break;
-                case 'R':
-                    n = n->right;
-                    break;
-                }
-            }
-        }
+    size_t lcm_value = 1;
+    for (const auto& p: paths) {
+        lcm_value = lcm(p, lcm_value);
     }
-    return step_count;
+    return lcm_value;
 }
 
 int main(int argc, char* argv[]) {
@@ -115,12 +157,14 @@ int main(int argc, char* argv[]) {
         }
         node->right = nodes.at(right);
     }
-    for (const auto& [name, node]: nodes) {
-        std::cout << std::format("Node {} = [{} , {}]\n", name, node->left->name, node->right->name);
-    }
-    size_t step_count = traverse(nodes, pattern);
-
-    ::printf("Task 1 result: %zu\n", step_count);
+    // for (const auto& [name, node]: nodes) {
+    //     std::cout << std::format("Node {} = [{} , {}]\n", name, node->left->name, node->right->name);
+    // }
+    std::cout << std::format("Node count {} - pattern size {} (prime - {})\n", nodes.size(), pattern.size(),
+                             is_prime(pattern.size()));
+    size_t step_count = count_paths(get_jumps(nodes, pattern));
+    step_count *= pattern.size();
+    ::printf("Task 2 result: %zu\n", step_count);
     return 0;
 }
 
