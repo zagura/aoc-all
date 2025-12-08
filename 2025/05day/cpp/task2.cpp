96cb3946104a2f60d8f5c1667aeeeee8076720ab
Michał Zagórski <zagura6@gmail.com>
2025-12-09 00:49:55 +0100

    AoC 2025: Day 5 - part 2 complete

--- 2025/05day/cpp/task2.cpp
+++ 2025/05day/cpp/task2.cpp
@@ -44,6 +44,33 @@ int main(int argc, char* argv[]) {
             ids.push_back(std::stoll(line));
         }
     }
+    std::vector<std::pair<long long int, long long int>> c_ranges {};
+    std::sort(ranges.begin(), ranges.end(), [](const auto& a, const auto& b) {
+        return ((a.first < b.first) or (a.first == b.first and a.second < b.second));
+    });
+    if (ranges.empty()) {
+        return 0;
+    }
+    std::pair<long long int, long long int> last_range = ranges.front();
+    // Already sorted by incrementing l
+    for (auto [l, r]: ranges) {
+        std::println("Old range: ({} - {})", l, r);
+        if (last_range.second < l) {
+            c_ranges.push_back(last_range);
+            last_range = {l, r};
+        } else {
+            if (r <= last_range.second) {
+                continue;
+            }
+            last_range.second = r;
+        }
+    }
+    c_ranges.push_back(last_range);
+    long long int total_range = 0;
+    for (auto [l, r]: c_ranges) {
+        std::println("Range [{}, {}]", l, r);
+        total_range += (r - l) + 1;
+    }
     for (auto [left, right]: ranges) {
         std::println("Range: {} to {}", left, right);
     }
@@ -57,9 +84,8 @@ int main(int argc, char* argv[]) {
             }
         }
     }
-    std::sort(ranges.begin(), ranges.end(), [](const auto& a, const auto& b) {
-        return ((a.first < b.first) or (a.first == b.first and a.second < b.second));
-    });
+
     ::printf("Task 1 result: %" PRIu64 "\n", total);
+    ::printf("Task 2 result: %" PRIu64 "\n", total_range);
     return 0;
 }
